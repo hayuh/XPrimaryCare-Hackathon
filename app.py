dc1e67bb682f7322a5c200e047ef3dca0973ae2e
hayuh <hyuh@andrew.cmu.edu>
2023-06-11 12:57:06 -0400

    Backend able to retrieve array of user input diagnostic codes.

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ import json
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+
+
 # Snowflake connection parameters
 account = 'ckb61329.prod3.us-west-2.aws'
 user = 'trentbuckholz'
@@ -130,10 +132,19 @@ print(return_med_prediction(['M25551', 'M79604']))
 # print ("Table created successfully")
 # conn.close()
 
-@app.route("/", methods = ['GET'])
+@app.route("/", methods = ['GET', 'POST'])
 def main_page():
   print("Handling request to home page.")
-  return render_template('test.html', diag_codes=diag_codes)
+  if request.method == 'GET':
+    return render_template('main.html', diag_codes=diag_codes)
+  elif request.method=='POST':
+      num_codes = request.form.get('num_codes')
+      submitted_diag_codes = []
+      for i in range(0, int(num_codes)):
+          submitted_diag_codes.append(request.form.get('input-box-' + str(i)))
+      print(num_codes)
+      print(submitted_diag_codes)
+      return render_template('main.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
